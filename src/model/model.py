bea567f7f2f949ad50c024411527bd1debc19082
Aadit <aadits2004@gmail.com>
2026-06-20 13:18:34 +0530

    causal self attn done

--- src/model/model.py
+++ src/model/model.py
@@ -1,25 +1,54 @@
-import torch 
+import torch
+from torch import Tensor
 import torch.nn as nn
 import torch.nn.functional as F
 import math
 from omegaconf import OmegaConf, DictConfig, ListConfig
-from dataclasses import dataclass
 
 model_cfg = OmegaConf.load("../../configs/model/LiteGPT-Small.yaml")
 
 
-@dataclass
-class ModelConfig:
-    d_model: int
-    n_layers: int
-    n_vocab: int
-    seq_len: int
-
-
-class CasualSelfAttention(nn.Module):
+class CausalSelfAttention(nn.Module):
     
-    def __init__(self) -> None:
+    def __init__(self, cfg: DictConfig | ListConfig = model_cfg) -> None:
         super().__init__()
+
+        self.config = cfg
+        self.dropout = cfg.dropout
+
+        self.n_head = self.config.n_head
+        self.d_model = self.config.d_model
+        self.head_dim = self.d_model // self.n_head
+
+        assert self.d_model % self.n_head == 0, "d_model / n_head should be divisible and give an int"
+
+        self.c_attn = nn.Linear(self.d_model, 3 * self.d_model)
+        self.c_proj = nn.Linear(self.d_model, self.d_model)
+
+    def forward(self, x: Tensor) -> Tensor:
+        B, T, C = x.shape
+
+        qkv = self.c_attn(x)
+        q, k, v = qkv.split(self.d_model, dim=2)
+
+        # (B, T, C) => (B, nh, T, hd)
+        q = q.view(B, T, self.n_head, self.head_dim).transpose(1, 2)
+        k = k.view(B, T, self.n_head, self.head_dim).transpose(1, 2)
+        v = v.view(B, T, self.n_head, self.head_dim).transpose(1, 2)
+
+        y = F.scaled_dot_product_attention(
+            q, 
+            k, 
+            v, 
+            is_causal=True,
+            dropout_p=self.dropout if self.training else 0.0
+        )
+
+        y = y.transpose(1, 2).contiguous().view(B, T, C)
+        y = self.c_proj(y)
+
+        return y
+
 
 
 class MLP(nn.Module):
@@ -32,7 +61,7 @@ class TransformerBlock(nn.Module):
 
     def __init__(self, cfg: DictConfig | ListConfig = model_cfg) -> None:
         super().__init__()
-        
+
 
 
 class LiteGPT(nn.Module):
