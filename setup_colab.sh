220fa14c164a3e2df643cfd2fe18441a226c7eec
Aadit <aadits2004@gmail.com>
2026-06-22 17:00:25 +0530

    setup file for colab

---
+++ setup_colab.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+
+echo "cloning repo..."
+git clone https://github.com/Aaidt/Lite-GPT.git
+
+echo "moving checkpoints/results/logs files into the repo..."
+for dir in checkpoints results logs; do
+    [ -d "$dir" ] && mv "$dir" Lite-GPT/
+done
+
+cd Lite-GPT
+
+echo "installing uv and other packages..."
+pip install -q uv 
+
+echo "Installing dependencies..."
+uv sync
+
+echo "Installing the shakespeare dataset..."
+cd src/data/ && uv run datasets.py
+
+echo "check installed dataset..."
+ls && cd ../..
+
+cat << EOF
+
+To log into wandb run:
+
+import wandb
+wandb.login(key="YOUR_API_KEY")
+
+Run training:
+
+uv run python -m src.training.train
+
+EOF
+
+
+echo "setup complete!!!"
