23bd0f0dd396b94c5bf8926dfcac8da5bcf64e8e
Aadit <aadits2004@gmail.com>
2026-06-23 14:03:29 +0530

    created the dataset to token to bin pipeline

---
+++ src/litegpt_25M/data/dataloader.py
@@ -0,0 +1,59 @@
+import torch
+from typing import Literal, Tuple
+from omegaconf import OmegaConf
+import numpy as np
+
+model_cfg = OmegaConf.load("./configs/model/LiteGPT-25M.yaml")
+train_cfg = OmegaConf.load("./configs/train/LiteGPT-25M.yaml")
+data_cfg = OmegaConf.load("./configs/data/LiteGPT-25M.yaml")
+
+class LiteGPTDataLoader:
+
+    def __init__(
+        self, 
+        split: Literal["train", "val"], 
+        batch_size: int = train_cfg.batch_size,
+        seq_len: int = model_cfg.seq_len
+    ) -> None:
+        
+        if split == "train":
+            path = data_cfg.train_bin
+        else:
+            path = data_cfg.val_bin
+            
+        self.batch_size = batch_size
+        self.seq_len = seq_len
+
+        self.data = np.memmap(
+            path,
+            dtype=np.uint16,
+            mode="r"
+        )
+
+    
+    def get_batch(self) -> Tuple[torch.Tensor, torch.Tensor]:
+        ix = torch.randint(
+            len(self.data) - self.seq_len,
+            (self.batch_size,)
+        )
+
+        x = torch.stack([
+            torch.from_numpy(
+                self.data[i:i+self.seq_len]
+                .astype(np.int64)
+            )
+            for i in ix.tolist()
+        ])
+
+        y = torch.stack([
+            torch.from_numpy(
+                self.data[i+1 : i+self.seq_len+1]
+                .astype(np.int64)
+            )
+            for i in ix.tolist()
+        ])
+
+        assert x.shape == (self.batch_size, self.seq_len), "x has the wrong tensor shape, it should (batch_size, seq_len)"
+        assert y.shape == (self.batch_size, self.seq_len), "y has the wrong tensor shape, it should (batch_size, seq_len)"
+
+        return x, y
